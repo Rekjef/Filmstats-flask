cc7efbbc001ae08490c40fb483d4ec3f3da0baf6
Filip Michalak <michalak.filip.04@gmail.com>
2020-10-03 09:24:30 +0200

    🤡 w1

--- source/app.py
+++ source/app.py
@@ -2,7 +2,57 @@ from flask import Flask, render_template, url_for, request, redirect
 import requests, random
 
 app = Flask(__name__)
-
+'''genres ={
+    
+    10759: {
+      "name": "Action & Adventure",
+      "color"
+    },
+    16:{
+      "name": "Animation"
+    },
+    35:{
+      "name": "Comedy"
+    },
+    80:{
+      "name": "Crime"
+    },
+    99:{
+      "name": "Documentary"
+    },
+    18:{
+      "name": "Drama"
+    },
+    10751:{
+      "name": "Family"
+    },
+    10762:{
+      "name": "Kids"
+    },
+9648,
+      "name": "Mystery"
+    },
+ 10763,
+      "name": "News"
+    },
+10764,
+      "name": "Reality"
+    },
+10765,
+      "name": "Sci-Fi & Fantasy"
+    },
+10766,
+      "name": "Soap"
+    },
+10767,
+      "name": "Talk"
+    },
+10768,
+      "name": "War & Politics"
+    },
+    37,
+      "name": "Western"
+    }'''
 apiKey = "d8bf019d0cca372bd804735f172f67e8"
 # ! https://api.themoviedb.org/3/tv/popular?api_key=d8bf019d0cca372bd804735f172f67e8&language=pl&page=1
 class MovieCard:
@@ -21,7 +71,7 @@ class MovieCard:
         #    self.totalSeasons = data['totalSeasons']
 
 class Movie:
-    def __init__(self, searchedDetails, searchedImagesList):
+    def __init__(self, searchedDetails, searchedImagesList, searchedActors):
         self.title = searchedDetails['name']
         self.titleOriginal = searchedDetails['original_name']
         self.released = searchedDetails['first_air_date']
@@ -29,12 +79,13 @@ class Movie:
         #self.runtime = data["Runtime"]
         self.director = ', '.join([x['name'] for x in searchedDetails['created_by']])
         #self.writer = data['Writer']
-        #self.actors = data['Actors']
+        self.actors = [{'name': x['name'], 'character_name': x['character'], 'image_id': x['profile_path']} for x in searchedActors['cast']]
         self.plot = searchedDetails['overview']
         self.poster = self.pickRandomPosterFromList(searchedImagesList, 'posters')
         self.backdrop_path = self.pickRandomPosterFromList(searchedImagesList, 'backdrops')
         self.ratings = {"rating": searchedDetails["vote_average"], "votes": searchedDetails["vote_count"]}
-        self.totalSeasons = len(searchedDetails['seasons'])
+        self.totalSeasons = searchedDetails['number_of_seasons']
+        self.totalEpisodes = searchedDetails['number_of_episodes']
         self.streamingPlatforms = [{'name': x['name'], 'logo': x['logo_path']}for x in searchedDetails['networks']]
         
     def pickRandomPosterFromList(self, searchedImagesList, typeOfImg: str):
@@ -54,7 +105,8 @@ def getInformationsAboutSpecificTvShow(title):
     id = getData(f'https://api.themoviedb.org/3/search/multi?api_key=d8bf019d0cca372bd804735f172f67e8&language=pl&query={title}&page=1&include_adult=false')['results'][0]['id']
     searchedDetails = getData(f'https://api.themoviedb.org/3/tv/{id}?api_key=d8bf019d0cca372bd804735f172f67e8&language=pl')
     searchedImagesList = getData(f'https://api.themoviedb.org/3/tv/{id}/images?api_key=d8bf019d0cca372bd804735f172f67e8')
-    return Movie(searchedDetails, searchedImagesList)
+    searchedActors = getData(f'https://api.themoviedb.org/3/tv/{id}/credits?api_key=d8bf019d0cca372bd804735f172f67e8')
+    return Movie(searchedDetails, searchedImagesList, searchedActors)
 
 
 
--- source/static/js/search.js
+++ source/static/js/search.js
@@ -2,3 +2,17 @@ function buttonClick(){
     var url = document.getElementById("inputURL").value;
     window.location = "/search/" + url;
 }
+
+//creates a listener for when you press a key
+window.onkeyup = keyup;
+
+//creates a global Javascript variable
+var inputTextValue;
+
+function keyup(e) {
+    inputTextValue = e.target.value;
+    console.log(inputTextValue);
+    if (e.keyCode == 13) {
+        window.location = "http://www.myurl.com/search/" + inputTextValue;
+    }
+}
